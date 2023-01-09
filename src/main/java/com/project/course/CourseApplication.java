525e340143a79a540a5df34c04724e573b632701
Lucas14tec <lucas.tecdantas@gmail.com>
2023-01-09 15:25:09 -0300

    Banco de dados H2

--- src/main/java/com/project/course/CourseApplication.java
+++ src/main/java/com/project/course/CourseApplication.java
@@ -2,6 +2,8 @@ package com.project.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
 public class CourseApplication {
--- src/main/java/com/project/course/resources/User.java
+++ src/main/java/com/project/course/resources/User.java
@@ -1,21 +1,22 @@
-package entities;
+package com.project.course.resources;
 
-import jakarta.persistence.Table;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.Order;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -23,8 +24,8 @@ public class User implements Serializable {
     private String password;
 
 
-    public User(){
 
+    public User() {
     }
 
     public User(Long id, String name, String email, String phone, String password) {
@@ -76,28 +77,31 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if(obj == null)
-            return true;
-        if (getClass() != obj.getClass())
-            return false;
-        User other = (User) obj;
-        if (id == null){
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
+
+
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id== null) ? 0 : id.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
