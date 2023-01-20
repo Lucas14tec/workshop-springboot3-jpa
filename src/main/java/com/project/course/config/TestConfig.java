9a419a43fbb3e884f199eda483a396042f87cf1a
Lucas14tec <lucas.tecdantas@gmail.com>
2023-01-20 00:59:09 -0300

    Payment, one-to-one association

--- src/main/java/com/project/course/config/TestConfig.java
+++ src/main/java/com/project/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.project.course.config;
 
+import com.project.course.entities.Payment;
 import com.project.course.entities.*;
 import com.project.course.entities.enums.OrderStatus;
 import com.project.course.repositories.*;
@@ -63,5 +64,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
--- src/main/java/com/project/course/entities/Order.java
+++ src/main/java/com/project/course/entities/Order.java
@@ -26,6 +26,8 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+    @OneToOne(mappedBy = "order" , cascade = CascadeType.ALL )
+    private Payment payment;
 
     public Order() {
     }
@@ -62,6 +64,16 @@ public class Order implements Serializable {
         if (orderStatus != null) {
             this.orderStatus = orderStatus.getCode();
         }
+    }
+
+
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems() {
